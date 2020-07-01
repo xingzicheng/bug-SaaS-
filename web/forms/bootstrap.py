bddeae8565dd5d0a6ad27b9fe5e11650df7754da
邢自成 <7732736+xing_zicheng@user.noreply.gitee.com>
2020-07-01 21:35:52 +0800

    项目展示

--- web/forms/bootstrap.py
+++ web/forms/bootstrap.py
@@ -1,6 +1,11 @@
 class BootStrapForm(object):
+    # 哪些标签不应用样式
+    bootstrap_class_exclude = []
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for name, field in self.fields.items():
+            if name in self.bootstrap_class_exclude:
+                continue
             field.widget.attrs['class'] = 'form-control'
             field.widget.attrs['placeholder'] = '请输入%s' % (field.label,)
--- web/urls.py
+++ web/urls.py
@@ -18,4 +18,8 @@ urlpatterns = [
     url(r'^logout/$', account.logout, name='logout'),
     # 项目管理
     url(r'^project/list/$', project.project_list, name='project_list'),
+    # /project/star/my/1
+    # /project/star/join/1
+    url(r'^project/star/(?P<project_type>\w+)/(?P<project_id>\d+)/$', project.project_star, name='project_star'),
+    url(r'^project/unstar/(?P<project_type>\w+)/(?P<project_id>\d+)/$', project.project_unstar, name='project_unstar'),
 ]
